fa89f167b0e7219ee4191faca3603e1b8da7d6cf
abdullohbahar <abdullohbahar@gmail.com>
2023-03-18 13:56:29 +0700

    add bendahara yayasan dashboard

--- app/Http/Livewire/Dashboard.php
+++ app/Http/Livewire/Dashboard.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire;
 use App\Models\AnakAsuh;
 use App\Models\Donation;
 use App\Models\Donatur;
+use App\Models\GoodsDonation;
+use App\Models\LksaFinance;
 use App\Models\Pengurus;
 use App\Models\Saving;
 use App\Models\TotalDanaDonation;
@@ -27,6 +29,9 @@ class Dashboard extends Component
             'donasiTunai' => Donation::where('jenis_donasi', 'Tunai')->whereMonth('tanggal_donasi', $monthNow)->whereYear('tanggal_donasi', $yearNow)->sum('pemasukan'),
             'donasiTransfer' => Donation::where('jenis_donasi', 'Transfer')->whereMonth('tanggal_donasi', $monthNow)->whereYear('tanggal_donasi', $yearNow)->sum('pemasukan'),
             'pengeluaran' => Donation::where('jenis_donasi', 'pengeluaran')->whereMonth('tanggal_donasi', $monthNow)->whereYear('tanggal_donasi', $yearNow)->sum('pengeluaran'),
+            'donasiBarang' => GoodsDonation::count(),
+            'pemaskuanLKSA' => LksaFinance::whereMonth('tanggal', $monthNow)->whereYear('tanggal', $yearNow)->sum('pemasukan'),
+            'pengeluaranLKSA' => LksaFinance::whereMonth('tanggal', $monthNow)->whereYear('tanggal', $yearNow)->sum('pengeluaran'),
         ];
 
         return view('livewire.dashboard', $data);
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -19,106 +19,11 @@
   <!-- Main content -->
   <section class="content">
     <div class="container-fluid">
-      <div class="row">
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-info" onclick="window.location='data-santri-dalam'">
-            <div class="inner">
-              <h4>{{ $santriDalam }}</h4>
-              <p>Santri Dalam</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-users"></i>
-            </div>
-            <a href="#" class="small-box-footer"></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-success" onclick="window.location='data-santri-luar'">
-            <div class="inner">
-              <h4>{{ $santriLuar }}</h4>
-
-              <p>Santri Luar</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-users"></i>
-            </div>
-            <a href="#" class="small-box-footer"></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-warning" onclick="window.location='donatur'">
-            <div class="inner">
-              <h4>{{ $total_donatur }}</h4>
-
-              <p>Total Donatur</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-users"></i>
-            </div>
-            <a href="#" class="small-box-footer"></a>
-          </div>
-        </div>
-        <!-- ./col -->
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-danger" onclick="window.location='/pengurus'">
-            <div class="inner">
-              <h4>{{ $pengurus }}</h4>
-
-              <p>Total Pengurus</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-users"></i>
-            </div>
-            <a href="#" class="small-box-footer"></a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-info" onclick="window.location='data-donasi-tunai'">
-            <div class="inner">
-              <h4>{{ "Rp " . number_format($donasiTunai, 2, ',', '.'); }}</h4>
-              <p>Total Donasi Tunai Bulan Ini</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-hand-holding-usd"></i>
-            </div>
-            <a href="#" class="small-box-footer"></a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-success" onclick="window.location='data-donasi-transfer'">
-            <div class="inner">
-              <h4>{{ "Rp " . number_format($donasiTransfer, 2, ',', '.'); }}</h4>
-              <p>Total Donasi Transfer Bulan Ini</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-hand-holding-usd"></i>
-            </div>
-            <a href="#" class="small-box-footer"></a>
-          </div>
-        </div>
-        <div class="col-lg-3 col-6">
-          <!-- small box -->
-          <div class="small-box bg-warning" onclick="window.location='/data-pengeluaran'">
-            <div class="inner">
-              <h4>{{ "Rp " . number_format($pengeluaran, 2, ',', '.'); }}</h4>
-              <p>Total Pengeluaran Bulan Ini</p>
-            </div>
-            <div class="icon">
-              <i class="fas fa-hand-holding-usd"></i>
-            </div>
-            <a href="#" class="small-box-footer"></a>
-          </div>
-        </div>
-        <!-- ./col -->
-      </div>
+      @if (Auth()->user()->role == 'ketua-yayasan' || Auth()->user()->role == 'admin-yayasan')
+        @include('livewire.dashboard-component.ketua-yayasan')
+      @elseif(Auth()->user()->role == 'bendahara-yayasan')
+        @include('livewire.dashboard-component.bendahara-yayasan')
+      @endif
     </div>
   </section>
   <!-- /.content -->
